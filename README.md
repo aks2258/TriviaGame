a669d4f8240174f408aad4c3ef7fb698e16ca54a
Arnie Kidd <Arnie@Arnies-MacDaddy.local>
2017-11-16 04:22:46 -0600

    Updated README and commented on files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TriviaGame
+
+A timed trivia game where a final score of questions answered correctly, incorrectly or not answered are displayed upon completion or no time left.
+
+Questions are hard-coded on the HTML page with the correct answer stored in the "answer" array in the app.js file that is used to check the answers the user chose.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,32 @@
+//Timer - Variables that will create a 40 second timer
 var number = 40;
 var intervalId;
 
+//Timer Start - Function that will start the timer
     function run() {
-      intervalId = setInterval(decrement, 1000);
+      intervalId = setInterval(decrement, 1000); //Timer will decrease by seconds
     }
-
+//Countdown - Timer will update on html file
     function decrement() {
       number--;
-      $("#show-number").html("<h2>Seconds left: " + number + "</h2>");
-      if (number === 0) {
-        stop();
-        alert("Time Up!");
-        check();
+      $("#show-number").html("<h2>Seconds left: " + number + "</h2>"); //will display how much time is left
+      if (number === 0) { //if timer is up...
+        stop(); //stops timer from going into negatives
+        alert("Time Up!"); //alerts user that time is up
+        check(); //calls the check function
       }
     }
 
     function stop() {
-       clearInterval(intervalId);
+       clearInterval(intervalId); //clears variable
     }
 
     
-    $("#start").click(run);
+    $("#start").click(run); //once "start" button is clicked run function (timer) is started
 
-
+//Answer check - Will check all the answers
 function check(){
+  //Variables will hold users answers
   var q1=document.quiz.question1.value;
   var q2=document.quiz.question2.value;
   var q3=document.quiz.question3.value;
@@ -35,22 +38,25 @@ function check(){
   var q9=document.quiz.question9.value;
   var q10=document.quiz.question10.value;
 
-  var incorrect=0;
-  var correct=0;
-  var unanswered=0;
-  var answers=["A", "B", "D", "B", "D", "C", "B", "A", "A", "C"];
+  var incorrect=0; //Will hold how many are incorrect
+  var correct=0; //Will hold how many are correct
+  var unanswered=0; //Will hold how many are unanswered
+  var answers=["A", "B", "D", "B", "D", "C", "B", "A", "A", "C"]; //Array holds the correct answers correlating correct order of questions
               //1   //2  //3  //4   //5 //6   //7 //8  //9  //10
+
+  //For Loop that will check each answer
   for (var i=1; i<=answers.length;i++){
     console.log(eval("q"+i));
-    if (eval("q"+i)==answers[i-1]){
-      correct++;
+    if (eval("q"+i)==answers[i-1]){ //if answer for question[i] is = answer[i-1] (because loop has already added 1)...
+      correct++; //correct answered is tallied
     }
-    else if (eval("q"+i)==null || eval("q"+i)==""){
-      unanswered++;
+    else if (eval("q"+i)==null || eval("q"+i)==""){ //Otherwise if it is unanswered...
+      unanswered++; //Question is unanswered
     }
   }
   
   console.log(unanswered);
+  //Results - Will only be visible once the user submits or time runs out. "Null" answers are subtracted from the total amount of questions, as are the correct answers, and the remaining number is the number of incorrect answers
   document.getElementById("after_submit").style.visibility="visible";
   document.getElementById("number_correct").innerHTML="You got "+correct+" correct, "+unanswered+" unanswered "+(answers.length-(correct+unanswered))+" incorrect";
 }
